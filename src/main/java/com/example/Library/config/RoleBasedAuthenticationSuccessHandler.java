1743487f8ce69cf4bd4a4bfa59f2d0dcd049a390
Alexandr-Frank <SACHAFRANK2004@yandex.ru>
2025-06-27 18:15:56 +0500

    Add full functions

---
+++ src/main/java/com/example/Library/config/RoleBasedAuthenticationSuccessHandler.java
@@ -0,0 +1,4 @@
+package com.example.Library.config;
+
+public class RoleBasedAuthenticationSuccessHandler {
+}
---
+++ src/main/java/com/example/Library/restController/LoginController.java
@@ -0,0 +1,4 @@
+package com.example.Library.restController;
+
+public class LoginController {
+}
